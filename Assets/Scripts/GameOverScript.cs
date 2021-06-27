9721a594da496405596eabb1cc4af6667c3090b8
eloinoel <eloi.sandt.games@gmx.de>
2021-06-27 17:16:50 +0200

    GameOver/GameWon Screen

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    
+    public void RetryButton() {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void ExitButton() {
+        //TODO: Call MainMenu Scene here
+    }
+}

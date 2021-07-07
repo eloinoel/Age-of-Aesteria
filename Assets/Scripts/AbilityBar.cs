0be93f5026132a0dcdd1f9dcd5b9ff96434fccb2
Brunolo <loenne@campus.tu-berlin.de>
2021-07-07 19:32:36 +0200

    Warnings hey, warnings yey

--- Assets/Scripts/AbilityBar.cs
+++ Assets/Scripts/AbilityBar.cs
@@ -9,6 +9,9 @@ public class AbilityBar : MonoBehaviour {
     public Text lightCounter;
     public Text fireCounter;
 
+    public Color grey;
+    public Color warnColor;
+
     private float sinceMeteor;
     private float sinceLight;
     private float sinceFire;
@@ -21,32 +24,45 @@ public class AbilityBar : MonoBehaviour {
     private bool lightOnCooldown = false;
     private bool fireOnCooldown = false;
 
+
+    private bool meteorWarning = false;
+    private bool lightWarning = false;
+    private bool fireWarning = false;
+    private float sinceMeteorWarning;
+    private float sinceLightWarning;
+    private float sinceFireWarning;
+    private float warningTime = 0.01f;
+
     void Start() {
         endAllCooldowns();
     }
 
     void Update() {
-        
+        handleCooldowns();
+        handleWarnings();
+    }
+
+    private void handleCooldowns() {
         // end cooldowns
-        if(Time.time - sinceMeteor < meteorTime && meteorOnCooldown) {
+        if (Time.time - sinceMeteor < meteorTime && meteorOnCooldown) {
             // decrement cooldowntime counter
-            meteorCounter.text = ((int) Mathf.Ceil(meteorTime - (Time.time - sinceMeteor))).ToString();
+            meteorCounter.text = ((int)Mathf.Ceil(meteorTime - (Time.time - sinceMeteor))).ToString();
             //setAlpha(transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>(), (120f*(1.0f - ((Time.time - sinceMeteor)/meteorTime)))/255f);
-        } else if(meteorOnCooldown) {
+        } else if (meteorOnCooldown) {
             endCooldown(1);
         }
 
-        if(Time.time - sinceLight < lightTime && lightOnCooldown) {
+        if (Time.time - sinceLight < lightTime && lightOnCooldown) {
             // decrement cooldowntime counter
-            lightCounter.text = ((int) Mathf.Ceil(lightTime - (Time.time - sinceLight))).ToString();
-        } else if(lightOnCooldown) {
+            lightCounter.text = ((int)Mathf.Ceil(lightTime - (Time.time - sinceLight))).ToString();
+        } else if (lightOnCooldown) {
             endCooldown(2);
         }
 
-        if(Time.time - sinceFire < fireTime && fireOnCooldown) {
+        if (Time.time - sinceFire < fireTime && fireOnCooldown) {
             // decrement cooldowntime counter
-            fireCounter.text = ((int) Mathf.Ceil(fireTime - (Time.time - sinceFire))).ToString();
-        } else if(fireOnCooldown) {
+            fireCounter.text = ((int)Mathf.Ceil(fireTime - (Time.time - sinceFire))).ToString();
+        } else if (fireOnCooldown) {
             endCooldown(3);
         }
     }
@@ -116,5 +132,69 @@ public class AbilityBar : MonoBehaviour {
         var tempColor = greyout.color;
         tempColor.a = alpha;
         greyout.color = tempColor;
+    }
+
+    private void handleWarnings() {
+        // end warnings
+        if(Time.time - sinceMeteorWarning >= warningTime && meteorWarning) {
+            endWarning(1);
+        }
+
+        if(Time.time - sinceLightWarning >= warningTime && lightWarning) {
+            endWarning(2);
+        }
+
+        if(Time.time - sinceFireWarning >= warningTime && fireWarning) {
+            endWarning(3);
+        }
+    }
+
+    public void setWarning(int ability) {
+        switch (cooldown) {
+            case 1:
+                // Meteor Warning
+                meteorWarning = true;
+                sinceMeteorWarning = Time.time;
+                //transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = warnColor;
+                break;
+            case 2:
+                // Light Warning
+                lightWarning = true;
+                sinceLightWarning = Time.time;
+                //transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = warnColor;
+                break;
+            case 3:
+                // Fire Warning
+                fireWarning = true;
+                sinceFireWarning = Time.time;
+                //transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = warnColor;
+                break;
+        }
+    }
+
+    public void endWarning(int ability) {
+        switch(ability) {
+            case 1:
+                // Meteor Warning
+                meteorWarning = false;
+                //transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = grey;
+                break;
+            case 2:
+                // Light Cooldown
+                lightWarning = false;
+                //transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = grey;
+                break;
+            case 3:
+                // Fire Cooldown
+                fireWarning = false;
+                //transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = grey;
+                break;
+        }
     }
 }
